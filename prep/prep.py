204286fabf0ebb8f4aa39597c36ccf399d94fda2
Humza Waheed <52869334+humzawaheed@users.noreply.github.com>
2024-08-05 12:10:50 +0000

    modified:   prep/prep.py 	new file:   ss.png

--- prep/prep.py
+++ prep/prep.py
@@ -8,10 +8,23 @@ import regex as re
 import matplotlib
 import seaborn
 import random
+from seleniumbase import BaseCase
+from seleniumbase import Driver
 
 class myCode():
     def humza(self):
         print("I love Pakistan!")
 
-A = myCode()
-A.humza()
+# A = myCode()
+# A.humza()
+
+class my_code(BaseCase):
+    def in_the_code(self):
+        with Driver() as driver:
+            driver.open("https://www.google.com")
+            driver.save_screenshot("ss.png")
+            print("successfull")
+
+# B = my_code()
+# B.in_the_code()
+
