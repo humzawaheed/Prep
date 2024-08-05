abbff99123c850d64d5c5d855ebe033b53f03124
Humza Waheed <52869334+humzawaheed@users.noreply.github.com>
2024-08-05 11:59:20 +0000

    modified:   prep/prep.py

---
+++ prep/prep.py
@@ -0,0 +1,17 @@
+import pandas
+import sklearn
+import seleniumbase
+import selenium
+import tensorflow
+import keras
+import regex as re
+import matplotlib
+import seaborn
+import random
+
+class myCode():
+    def humza(self):
+        print("I love Pakistan!")
+
+A = myCode()
+A.humza()
